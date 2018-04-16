abdb0594fae46db2f3b11d6d149f678fef81da98
Iker Garcia <flipas.garcia@gmail.com>
2018-04-17 01:00:48 +0200

    ADDED angular 4 first aprox ADDED menu first template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
+import { MainComponent } from './components/main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
